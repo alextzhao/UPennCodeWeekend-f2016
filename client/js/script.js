c98b98cbb4be56564d934e5acdb4f86307958241
Alex Zhao <alexzhao@seas.upenn.edu>
2016-09-04 11:52:20 -0400

    modifies script.js to set up rudimentary server

--- client/js/script.js
+++ client/js/script.js
@@ -1,3 +1,22 @@
-function add(x, y) {
-	return x + y;
-}
+'use strict';
+
+let Server = require('socket.io');
+let io = Server(3000);
+console.log("SocketIO listening on 3000");
+
+//Socket Handler Functions
+io.on('connection', (socket) => {
+	console.log("GOt a new Connection");
+
+	//Socket Handers
+	/*
+		Handles PING
+		Responds with PONG
+	 */
+
+	socket.on('PING', (data) => {
+		console.log("Got a PING");
+
+		socket.emit('PONG'); // reply with PONG
+	})
+})

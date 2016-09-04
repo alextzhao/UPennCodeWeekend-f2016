ca03acf90f03e4544a9c840d01ba8fc99aa18fce
Alex Zhao <alexzhao@seas.upenn.edu>
2016-09-04 12:33:54 -0400

    adds helper functions

--- client/js/socket-client.js
+++ client/js/socket-client.js
@@ -1,6 +1,17 @@
 'use strict';
 var socket = io('http://localhost:3000');
 
+// stores user in channel. It's a dictionary with id as key
+// User objects have these fields
+//   - id     (user id)
+//   - name   (user name)
+//   - string (string representation)
+var users = {};
+
+// stores the client user id
+// hence users[me] or users.me gives the client user
+var me = socket.id;
+
 socket.emit('PING');
 
 // ---------------------
@@ -9,3 +20,27 @@ socket.emit('PING');
 socket.on('PONG', function (data) {
   console.log('Got PONG!');
 });
+
+// ---------------
+//     HELPERS
+// ---------------
+
+/**
+ * Showcases functional Javascript (_.fold) and ternary operators
+ * to get a list of the users currently chatting
+ */
+function getUserList() {
+  return _.reduce(users,
+                  function (rest, user) {
+                    return (rest ? rest + ', ' : '') + user.name;
+                  },
+                  ''
+                 );
+}
+
+/**
+ * Sends a MESG to the server
+ */
+function sendMessage(message) {
+  socket.emit('MESG', {message: message});
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 'use strict'; // puts us in strict mode (js in hard mode)
 
+// gets imports
+let Moniker = require('moniker');
+let _ = require('underscore');
+
+let users = {};
+
 /* This sets up a pure socket-io server.
  * Later in the guide we upgrade to a full
  * express server, to serve files as well
@@ -27,3 +33,27 @@ io.on('connection', (socket) => {
     socket.emit('PONG'); // reply with a PONG
   });
 });
+
+// HELPER FUNCTIONS
+/**
+ * Sees if a name is unique
+ * @param name The name to check
+ * @return boolean true if the name is unique
+ */
+function isUniqueName(name) {
+  let names = _.mapObject(users, (user) => user.getName().toLowerCase());
+  return !_.contains(names, name.toLowerCase());
+}
+
+/**
+ * Gets a unique name using Moniker (showcases basic npm modules)
+ * @return String a unique name
+ */
+function getUniqueName() {
+  let name = Moniker.choose();
+  while(!isUniqueName(name)) {
+    name = Moniker.choose();
+  }
+
+  return name;
+}
